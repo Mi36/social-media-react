f6fdc1bb93a765fefc2b33735eaa46f896425f55
Illyas <illyas.deulect@gmail.com>
2021-05-19 00:24:44 +0530

    updated config part

---
+++ config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  MONGODB:
+    "mongodb+srv://classsed:6lxxxlexaTslHLPe@cluster0-pcsru.mongodb.net/merng?retryWrites=true",
+  SECRET_KEY: "some very secret key",
+};
+
+//add the real mongodb string here fro dashboard
+// clusters->connect->connect your application->copy string from there-->add corresponding password and userid also
+//username and a passwor of user we created database access section
